456e79f53004c9d7bdd4d4d9e8ff33d9a11b45bd
Monk <tbsanders5@gmail.com>
2021-03-08 16:17:57 -0800

    added components, installed rendering packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Portfolio from "./Components/Portfolio";
+import Pricing from "./Components/Pricing";
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='allButFooter'>
+        {/* <p>WORK!!!!</p> */}
+        <Router>
+          {/* <Header /> */}
+          <div>
+            <Route exact path="/" component={About} />
+            {/* <Route exact path={process.env.PUBLIC_URL + "/contact"} component={Contact} /> */}
+            {/* <Route exact path={process.env.PUBLIC_URL + "/portfolio"} component={Portfolio} /> */}
+            {/* <Route exact path={process.env.PUBLIC_URL + "/pricing"} component={Pricing} /> */}
+          </div>
+        </Router>
+      </div>
+      {/* <footer id='footer' className='bg-dark text-white-50'>
+        <Footer />
+      </footer> */}
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+function About (){
+    return (
+        <section className='main'>
+            <div className='container'>
+                <div className="row">
+                    <header className='col-12'><h2>Golden Hour Photography</h2></header>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;

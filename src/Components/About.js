03632339126d77a2495804a8120908286317c0d1
Monk <tbsanders5@gmail.com>
2021-03-15 14:32:35 -0700

    added images, working on about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,10 @@ function About (){
     return (
         <section className='main'>
             <div className='container'>
+                <img className="responsive-img" alt="sunset-cliffs" src="/assets/images/Love5.jpg"></img>
                 <div className="row">
                     <header className='col-12'><h2>Golden Hour Photography</h2></header>
+                    <p>I am a San Diego based lifestyle photographer!</p>
                 </div>
             </div>
         </section>

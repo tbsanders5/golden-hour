2facd47fb9f78bf289231834f281ce72951fbbe1
Monk <tbsanders5@gmail.com>
2021-03-22 16:55:29 -0700

    fixed background image

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,13 @@
+import React from 'react';
+import "../Components/About.css"
+
 function About (){
     return (
-        <section className='main'>
-            <div className='container'>
-                <img className="responsive-img" alt="sunset-cliffs" src="/assets/images/Love5.jpg"></img>
-                <div className="row">
-                    <header className='col-12'><h2>Golden Hour Photography</h2></header>
+        <div> 
+                {/* <img className="responsive-img" alt="sunset-cliffs" src="/assets/images/Love5.jpg"></img> */}
+                    <header className='center-align homepage-title'><h2>Golden Hour Photography</h2></header>
                     <p>I am a San Diego based lifestyle photographer!</p>
-                </div>
-            </div>
-        </section>
+        </div>
     )
 }
 
